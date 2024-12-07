b07d612ce1584f9916cb19ccdfec7f9ae7125e39
Travis Beck <travis@travisjbeck.com>
2024-12-07 14:28:11 -0700

    wezterm updates

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -9,9 +9,9 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 16
 
-config.enable_tab_bar = false
+config.enable_tab_bar = true
 
-config.window_decorations = "TITLE | RESIZE"
+config.window_decorations = "RESIZE"
 
 -- config.color_scheme = "Tokyo Night Moon"
 -- config.color_scheme = "Poimandres"
