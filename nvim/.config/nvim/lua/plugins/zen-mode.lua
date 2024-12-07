a262f83d28853e061f5dcfd3a9e2d5d431fbfeaa
Travis Beck <travis@travisjbeck.com>
2024-12-07 14:43:42 -0700

    zen mode updates

--- nvim/.config/nvim/lua/plugins/zen-mode.lua
+++ nvim/.config/nvim/lua/plugins/zen-mode.lua
@@ -33,52 +33,30 @@ return {
         -- statusline will be shown only if 'laststatus' == 3
         laststatus = 0, -- turn off the statusline in zen mode
       },
+      diagnostics = { enabled = false },
       twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
       gitsigns = { enabled = false }, -- disables git signs
       tmux = { enabled = false }, -- disables the tmux statusline
       todo = { enabled = false }, -- if set to "true", todo-comments.nvim highlights will be disabled
-      -- this will change the font size on kitty when in zen mode
-      -- to make this work, you need to set the following kitty options:
-      -- - allow_remote_control socket-only
-      -- - listen_on unix:/tmp/kitty
-      kitty = {
-        enabled = false,
-        font = "+4", -- font size increment
-      },
-      -- this will change the font size on alacritty when in zen mode
-      -- requires  Alacritty Version 0.10.0 or higher
-      -- uses `alacritty msg` subcommand to change font size
-      alacritty = {
-        enabled = false,
-        font = "14", -- font size
-      },
-      -- this will change the font size on wezterm when in zen mode
-      -- See alse also the Plugins/Wezterm section in this projects README
-      wezterm = {
-        enabled = false,
-        -- can be either an absolute font size or the number of incremental steps
-        font = "+4", -- (10% increase per step)
-      },
-      -- this will change the scale factor in Neovide when in zen mode
-      -- See alse also the Plugins/Wezterm section in this projects README
-      neovide = {
-        enabled = false,
-        -- Will multiply the current scale factor by this number
-        scale = 1.2,
-        -- disable the Neovide animations while in Zen mode
-        disable_animations = {
-          neovide_animation_length = 0,
-          neovide_cursor_animate_command_line = false,
-          neovide_scroll_animation_length = 0,
-          neovide_position_animation_length = 0,
-          neovide_cursor_animation_length = 0,
-          neovide_cursor_vfx_mode = "",
-        },
-      },
     },
     -- callback where you can add custom code when the Zen window opens
-    on_open = function(win) end,
-    -- callback where you can add custom code when the Zen window closes
-    on_close = function() end,
+    -- Store the diagnostic state
+    on_open = function()
+      -- Store current diagnostic state
+      vim.g.zen_diagnostics_enabled = vim.diagnostic.is_disabled()
+      -- Disable diagnostics
+      vim.diagnostics.enable(false)
+      vim.opt.wrap = true
+    end,
+    on_close = function()
+      -- Restore previous diagnostic state
+      if not vim.g.zen_diagnostics_enabled then
+        vim.diagnostic.enable()
+      end
+      vim.opt.wrap = false
+    end,
+  },
+  keys = {
+    { "<leader>z", "<cmd>ZenMode<cr>", desc = "Toggle Zen Mode" },
   },
 }
