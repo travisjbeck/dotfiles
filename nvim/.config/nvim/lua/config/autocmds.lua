0da9b402ce804bd491dd6c6b77d7c1812b17ea00
Travis Beck <travis@travisjbeck.com>
2024-12-08 14:46:55 -0700

    update kitty tab bar, wezterm fiddling, got fzf installed for zsh

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "IncSearch",
+      timeout = 100,
+    })
+  end,
+})
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,30 +7,6 @@ require("on")
 local schemeName = "Tokyo Night"
 local scheme = wezterm.get_builtin_color_schemes()[schemeName]
 
--- Allow neovim zen mode to change the font size
-wezterm.on("user-var-changed", function(window, pane, name, value)
-	local overrides = window:get_config_overrides() or {}
-	if name == "ZEN_MODE" then
-		local incremental = value:find("+")
-		local number_value = tonumber(value)
-		if incremental ~= nil then
-			while number_value > 0 do
-				window:perform_action(wezterm.action.IncreaseFontSize, pane)
-				number_value = number_value - 1
-			end
-			overrides.enable_tab_bar = false
-		elseif number_value < 0 then
-			window:perform_action(wezterm.action.ResetFontSize, pane)
-			overrides.font_size = nil
-			overrides.enable_tab_bar = true
-		else
-			overrides.font_size = number_value
-			overrides.enable_tab_bar = false
-		end
-	end
-	window:set_config_overrides(overrides)
-end)
-
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
@@ -79,6 +55,15 @@ config.colors = {
 
 		inactive_tab = { bg_color = scheme.background, fg_color = "#FCE8C3" },
 		inactive_tab_hover = { bg_color = scheme.ansi[1], fg_color = "#FCE8C3" },
+	},
+}
+
+config.keys = {
+	-- Disable Ctrl-D by making it do nothing
+	{
+		key = "d",
+		mods = "CTRL",
+		action = wezterm.action.DisableDefaultAssignment,
 	},
 }
 
