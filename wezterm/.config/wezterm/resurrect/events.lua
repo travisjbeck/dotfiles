4dbc19d4bd5194c65b23a89a8330bb39cb33d3cf
Travis Beck <travis@travisjbeck.com>
2024-12-09 14:44:57 -0700

    working on wezterm notifications

--- wezterm/.config/wezterm/resurrect/events.lua
+++ wezterm/.config/wezterm/resurrect/events.lua
@@ -4,6 +4,7 @@ local suppress_notification = false
 
 local function getWindow()
 	local windows = wezterm.gui.gui_windows()
+	wezterm.log_info(windows)
 	for _, window in ipairs(windows) do
 		if window:is_focused() then
 			return window
@@ -18,16 +19,20 @@ wezterm.on("resurrect.error", function(error)
 	local window = getWindow()
 	wezterm.log_info("window object: " .. tostring(window))
 	if window then
-		window:toast_notification("Wezterm - ERROR", error)
+		window:toast_notification("Wezterm - ERROR", error, nil, 4000)
 	end
 end)
 
 wezterm.on("resurrect.periodic_save", function()
+	wezterm.log_info("periodic save")
 	suppress_notification = true
 end)
 
 wezterm.on("resurrect.save_state.finished", function(session_path)
+	wezterm.log_info("resurrect saved")
+
 	local window = getWindow()
+	wezterm.log_info(window)
 	local is_workspace_save = session_path:find("state/workspace")
 	if is_workspace_save == nil then
 		return
@@ -37,16 +42,16 @@ wezterm.on("resurrect.save_state.finished", function(session_path)
 		return
 	end
 	local path = session_path:match(".+/([^+]+)$")
-	local name = path:match("^(.+)%.json$")
 	if window then
-		window:toast_notification("Wezterm - Save workspace", "Saved workspace " .. name .. "\n\n" .. session_path)
+		window:toast_notification("Wezterm", "Saved workspace", nil, 4000)
 	end
+	local name = path:match("^(.+)%.json$")
 end)
 
 wezterm.on("resurrect.load_state.finished", function(name, type)
 	local msg = "Completed loading " .. type .. " state: " .. name
 	local window = getWindow()
 	if window then
-		window:toast_notification("Wezterm - Restore session", msg)
+		window:toast_notification("Wezterm - Restore session", msg, nil, 4000)
 	end
 end)
